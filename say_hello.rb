6af8285a316e4b2e487094c813b655df47cc193e
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-10-05 17:06:46 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,5 @@
 # Build your say_hello method here
-require_relative 'spec/spec_helper.rb'
-require_relative 'spec/say_hello_spec.rb'
+
 def say_hello(name, "Ruby programmer")
   puts "Hello #{name}!"
 end
